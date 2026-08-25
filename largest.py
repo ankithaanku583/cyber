5775db777c31058811f042570a886fc7c33731ac
ankithaanku583 <ankithaanku583@gmail.com>
2026-08-25 12:17:50 +0530

    Create largest.py

---
+++ largest.py
@@ -0,0 +1,8 @@
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+num3 = float(input("Enter third number: "))
+
+# Conditional logic to find the largest number
+if (num1 >= num2) and (num1 >= num3):
+    largest = num1
+elif (num2 >= 
